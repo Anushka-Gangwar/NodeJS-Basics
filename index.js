048edae7d33942bf178050487abf0647a1c85701
Anushka Gangwar <anushkagangwar475@gmail.com>
2026-07-26 10:09:05 +0530

    Added JavaScript variables practice

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log("My name is Anushka.");
-console.log("I am learning Node.js.");
-console.log("This is my first JavaScript program.");
+let name = "Anushka";
+let age = 23;
+let college = "MIT-WPU";
+let city = "Pune";
+let language = "C++";
+let goal = "Software Engineer";
+let favoriteFood = "Pizza";
+let hobby = "dancing" ;
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old.");
+console.log("I study at " + college + ".");
+console.log("I live in " + city + ".");
+console.log("I know " + language + ".");
+console.log("My goal is to become a " + goal + ".");
+console.log("My favourite food is "+ favoriteFood);
+console.log("hobby is "+ hobby);
